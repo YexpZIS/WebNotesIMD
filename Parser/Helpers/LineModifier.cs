52a0b654924a024eb8b218ea997096af7e2332fa
Den <you@example.com>
2021-01-08 16:23:52 +0300

    add LineModifier.class that insert tags in line

---
+++ Parser/Helpers/LineModifier.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parser.Helpers
+{
+    public class LineModifier
+    {
+        private string result = "";
+        private string text = "";
+        private string delimiter = "````";
+
+        private int index = 0;
+        private int length = 0;
+
+        /// <param name="tags">_tag.tags[Tag.InlineCode][0] or "<inline-code>{0}</inline-code>"</param>
+        public string InsertTagsInLine(string line, string tags, string delimiter = "````")
+        {
+            ClearValues();
+
+            this.delimiter = delimiter;
+
+            while ((index = findIndex(line)) != -1)
+            {
+                // Text before delimiter
+                result += getText(line);
+                line = RemoveTextAndDelimiter(line);
+
+                // Text between delimiters
+                index = findIndex(line);
+                text = getText(line);
+                result += getHtml(tags);
+                line = RemoveTextAndDelimiter(line);
+            }
+
+            result += line;
+
+            return result;
+        }
+        private string getHtml(string tags)
+        {
+            return string.Format(tags, text);
+        }
+
+        private string getText(string line)
+        {
+            if (index == -1)
+            {
+                return line;
+            }
+            return line.Substring(0, index);
+        }
+
+        private int findIndex(string line)
+        {
+            return line.IndexOf(delimiter);
+        }
+
+        private string RemoveTextAndDelimiter(string text)
+        {
+            if (index == -1)
+            {
+                return "";
+            }
+            else
+            {
+                length = index + delimiter.Length;
+                return text.Substring(length, text.Length - length);
+            }
+        }
+
+        private void ClearValues()
+        {
+            result = "";
+            text = "";
+            delimiter = "````";
+
+            index = 0;
+            length = 0;
+        }
+    }
+}
---
+++ Tests/Parser/Helpers/LineModifier_Tests.cs
@@ -0,0 +1,156 @@
+﻿
+
+using NUnit.Framework;
+using Parser.Helpers;
+using Parser.Tags;
+
+namespace Tests.Parser.Helpers
+{
+    class LineModifier_Tests
+    {
+        private LineModifier _modifier;
+        private ITag _tag;
+
+        private string inlineCodeTags;
+
+        private string text;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _modifier = new LineModifier();
+            _tag = new TestTags();
+
+            inlineCodeTags = _tag.tags[Tag.InlineCode][0];
+        }
+
+        [Test]
+        public void SimpleTest()
+        {
+            // Arrange
+            string line = "````text ````";
+            // Act
+            text = _modifier.InsertTagsInLine(line, inlineCodeTags);
+            // Assert
+            Assert.AreEqual("<inline-code>text </inline-code>", text);
+        }
+
+
+        // TagAreNotClosed
+
+        [Test]
+        public void TagAreNotClosed_AtTheBeginning()
+        {
+            // Arrange
+            string line = "````text df";
+            // Act
+            text = _modifier.InsertTagsInLine(line, inlineCodeTags);
+            // Assert
+            Assert.AreEqual("<inline-code>text df</inline-code>", text);
+        }
+
+        [Test]
+        public void TagAreNotClosed_InTheMiddle()
+        {
+            // Arrange
+            string line = "66022-2-2yyyy ````text todm, as,fmgnafd,gmn";
+            // Act
+            text = _modifier.InsertTagsInLine(line, inlineCodeTags);
+            // Assert
+            Assert.AreEqual("66022-2-2yyyy <inline-code>text todm, as,fmgnafd,gmn</inline-code>", text);
+        }
+
+        [Test]
+        public void TagAreNotClosed_InTheEnd()
+        {
+            // Arrange
+            string line = "some this text ````";
+            // Act
+            text = _modifier.InsertTagsInLine(line, inlineCodeTags);
+            // Assert
+            Assert.AreEqual("some this text <inline-code></inline-code>", text);
+        }
+
+        
+        // Quantity
+
+        [Test]
+        public void One_TagInText()
+        {
+            // Arrange
+            string line = "asdfasdf````text ````asdfasdf";
+            // Act
+            text = _modifier.InsertTagsInLine(line, inlineCodeTags);
+            // Assert
+            Assert.AreEqual("asdfasdf<inline-code>text </inline-code>asdfasdf", text);
+        }
+
+        [Test]
+        public void Two_TagsInText()
+        {
+            // Arrange
+            string line = "asdfasdf````text````asdfasdf````tag````";
+            // Act
+            text = _modifier.InsertTagsInLine(line, inlineCodeTags);
+            // Assert
+            Assert.AreEqual("asdfasdf<inline-code>text</inline-code>asdfasdf<inline-code>tag</inline-code>", text);
+        }
+
+        [Test]
+        public void Many_TagsInText()
+        {
+            // Arrange
+            string line = "dsdf````text ````asd````f````````a````sd````f`````";
+            // Act
+            text = _modifier.InsertTagsInLine(line, inlineCodeTags);
+            // Assert
+            Assert.AreEqual("dsdf<inline-code>text </inline-code>asd<inline-code>f</inline-code><inline-code>a</inline-code>sd<inline-code>f</inline-code>`", text);
+        }
+
+        [Test]
+        public void No_TagsInText()
+        {
+            // Arrange
+            string line = "asdfasdf`text asdfasdf";
+            // Act
+            text = _modifier.InsertTagsInLine(line, inlineCodeTags);
+            // Assert
+            Assert.AreEqual("asdfasdf`text asdfasdf", text);
+        }
+
+        // Different delimitors
+
+        [Test]
+        public void Delimitor_OneApostrophe()
+        {
+            // Arrange
+            string line = "asdfasdf`text`asdfasdf";
+            // Act
+            text = _modifier.InsertTagsInLine(line, inlineCodeTags, "`");
+            // Assert
+            Assert.AreEqual("asdfasdf<inline-code>text</inline-code>asdfasdf", text);
+        }
+
+        [Test]
+        public void Delimitor_TwoStars()
+        {
+            // Arrange
+            string line = "asd**fasdf`text`asdfa**sdf";
+            // Act
+            text = _modifier.InsertTagsInLine(line, inlineCodeTags, "**");
+            // Assert
+            Assert.AreEqual("asd<inline-code>fasdf`text`asdfa</inline-code>sdf", text);
+        }
+
+        [Test]
+        public void EmptyLine()
+        {
+            // Arrange
+            string line = "";
+            // Act
+            text = _modifier.InsertTagsInLine(line, inlineCodeTags);
+            // Assert
+            Assert.AreEqual("", text);
+        }
+    }
+}
